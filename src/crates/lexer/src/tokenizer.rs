2825adfe47d4d1ba6b9a4f557c094e40ab773bf1
vlinearo <vlinearo@gmail.com>
2025-11-30 00:12:14 +0200

    tokenizer

--- src/crates/lexer/src/tokenizer.rs
+++ src/crates/lexer/src/tokenizer.rs
@@ -1,17 +1,60 @@
+use std::fmt;
 
+use db::db;
+use regex::Regex;
 
-
-#[derive(Clone)]
-pub struct Tokenizer {
+#[derive(Debug)]
+pub struct Token {
+    pub value: String,
+    pub token_type: String,
 }
 
+pub struct Parser {
+    db: db::DB,
+}
 
-impl Tokenizer {
-  pub fn new() -> Self {
-    Self {  }
+impl std::fmt::Display for Token {
+  fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+    write!(f, "{}", self)
   }
+}
 
-  pub fn tokenize(&self) {
+impl Parser {
+    pub fn new(db: db::DB) -> Self {
+        Parser { db }
+    }
 
-  }
+    pub fn parse(&mut self, input: &str) -> Vec<Token> {
+        let mut tokens = Vec::new();
+        
+        let re = Regex::new(r"[a-zA-Zа-яА-ЯіїєґІЇЄҐ0-9_]+|[^\s\w]").unwrap();
+
+        for mat in re.find_iter(input) {
+            let lexeme_str = mat.as_str();
+            
+            let db_result = self.db.select_lexem(lexeme_str);
+            let token_type: String = if !db_result.is_empty() {
+                db_result[0].clone()
+            } else {
+                self.guess_type(lexeme_str)
+            };
+
+            tokens.push(Token {
+                value: lexeme_str.to_string(),
+                token_type,
+            });
+        }
+
+        tokens
+    }
+
+    fn guess_type(&self, lexeme: &str) -> String {
+        if lexeme.chars().all(|c| c.is_numeric()) {
+            "Number".to_string()
+        } else if lexeme.chars().next().unwrap().is_alphabetic() {
+            "Identifier".to_string()
+        } else {
+            "UnknownSymbol".to_string()
+        }
+    }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,6 @@
 use db::{self, db::DB};
+use lexer::tokenizer;
+
 use dotenvy::dotenv;
 use std::env;
 
@@ -8,5 +10,9 @@ fn main() {
     let mut _db = DB::new(
         env::var("DATABASE_URL")
                      .expect("DATABASE_URL must be set").as_str(), );
-    println!("{:?}", _db.select_lexem("змінна"));
+    let mut parser = tokenizer::Parser::new(_db);
+    let tokens = parser.parse("1 2234 131 функція нехай змінна змінна число");
+    for el in tokens {
+        println!("{:?}", el);
+    }
 }
